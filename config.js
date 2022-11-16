34c0f45f1cdf0abed5f7560973fab1bb090384bd
Bellfroutcs <87676463+Bellfroutcs@users.noreply.github.com>
2022-11-17 02:15:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,28 +7,28 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.Owner = ['6283816797646']
+global.OwnerNumber = ['6283816797646']
+global.ownertag = ['6283816797646']
+global.BotName = "Miku BOTz"
+global.packname = "Miku BOTz"
+global.author = "By Ananda DEV"
+global.OwnerName = "AnandaDEV"
+global.BotSourceCode = "PRIVATE"
+global.SupportGroupLink = "https://chat.whatsapp.com/KGui5W4Ud4n224ZLMqR0rZ"
 global.sessionName = "session"
 
 // Prefix //
 // If you want to change the prefix, change it and also change all switch's button id from Core.js  otherwise buttons will not work //
-global.prefa = ['-'] 
+global.prefa = ['.'] 
 
 
-global.location = "West Bengal, India"
+global.location = "Medan, Indonesia"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
-global.lolhuman = "KaysaS"
+global.websitex = "https://youtube.com/channel/UCL_UuXsDGFLdwARCwytYFBA"
+global.lolhuman = "NdaDev"
 
 
 global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
